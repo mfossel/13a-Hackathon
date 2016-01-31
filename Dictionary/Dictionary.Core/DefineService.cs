a27b622c581c660591cd6d972760566f851e1f63
Mike Fossel <mikefossel@gmail.com>
2016-01-30 16:36:35 -0800

    Added try/catch

--- Dictionary/Dictionary.Core/DefineService.cs
+++ Dictionary/Dictionary.Core/DefineService.cs
@@ -18,76 +18,98 @@ namespace Dictionary.Core
         public static string YodaInput = "";
         public static string GetDef(string word)
         {
-            string def;
-            string definition = "";
-            WebRequest request = WebRequest.Create($"https://montanaflynn-dictionary.p.mashape.com/define?word={word}");
-            request.Headers.Add("X-Mashape-Key", "s9SQZi4fYCmshpkI3i0IYEYg4m8hp13rW2DjsnALWln7XkT8Ez");
-
-            WebResponse response = request.GetResponse();
-            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            try
             {
-                def = streamReader.ReadToEnd();
+
+
+
+                string def;
+                string definition = "";
+                WebRequest request = WebRequest.Create($"https://montanaflynn-dictionary.p.mashape.com/define?word={word}");
+                request.Headers.Add("X-Mashape-Key", "s9SQZi4fYCmshpkI3i0IYEYg4m8hp13rW2DjsnALWln7XkT8Ez");
+
+                WebResponse response = request.GetResponse();
+                using (var streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    def = streamReader.ReadToEnd();
+                }
+                var o = JObject.Parse(def);
+                string s = o["definitions"].ToString();
+
+                List<Definition> deflist = new List<Definition>();
+                deflist = JsonConvert.DeserializeObject<List<Definition>>(s);
+
+                for (int i = 0; i < 4; i++)
+                {
+                    definition += "Definition: " + deflist[i].text + "\n\n";
+                    definition += "Attribution: " + deflist[i].attribution + "\n\n";
+                }
+                return definition;
             }
-            var o = JObject.Parse(def);
-            string s = o["definitions"].ToString();
-
-            List<Definition> deflist = new List<Definition>();
-            deflist = JsonConvert.DeserializeObject<List<Definition>>(s);
-
-            for (int i = 0; i < 4; i++)
+            catch
             {
-                definition += "Definition: " + deflist[i].text + "\n\n";
-                definition += "Attribution: " + deflist[i].attribution + "\n\n";
+                return "No word found.";
             }
-
-
-            return definition;
+            
 
         }
 
         public static string GetUrbanDef(string word)
         {
-            string def;
-            string definition = "";
-            WebRequest request = WebRequest.Create($"https://mashape-community-urban-dictionary.p.mashape.com/define?term={word}");
-            request.Headers.Add("X-Mashape-Key", "s9SQZi4fYCmshpkI3i0IYEYg4m8hp13rW2DjsnALWln7XkT8Ez");
-
-            WebResponse response = request.GetResponse();
-            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            try
             {
-                def = streamReader.ReadToEnd();
+
+                string def;
+                string definition = "";
+                WebRequest request = WebRequest.Create($"https://mashape-community-urban-dictionary.p.mashape.com/define?term={word}");
+                request.Headers.Add("X-Mashape-Key", "s9SQZi4fYCmshpkI3i0IYEYg4m8hp13rW2DjsnALWln7XkT8Ez");
+
+                WebResponse response = request.GetResponse();
+                using (var streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    def = streamReader.ReadToEnd();
+                }
+                var o = JObject.Parse(def);
+                string s = o["list"].ToString();
+
+                List<UrbanDef> deflist = new List<UrbanDef>();
+                deflist = JsonConvert.DeserializeObject<List<UrbanDef>>(s);
+
+                YodaInput = deflist[0].example;
+                for (int i = 0; i < 4; i++)
+                {
+                    definition += "Definition: " + deflist[i].definition + "\n\n";
+                    definition += "Example: " + deflist[i].example + "\n\n";
+                }
+
+
+                return definition;
             }
-            var o = JObject.Parse(def);
-            string s = o["list"].ToString();
-
-            List<UrbanDef> deflist = new List<UrbanDef>();
-            deflist = JsonConvert.DeserializeObject<List<UrbanDef>>(s);
-
-            YodaInput = deflist[0].example;
-            for (int i = 0; i < 4; i++)
+            catch
             {
-                definition += "Definition: " + deflist[i].definition + "\n\n";
-                definition += "Example: " + deflist[i].example + "\n\n";
+                return "No word found.";
             }
-
-
-            return definition;
-
         }
 
         public static string YodaTalk(string sent)
         {
-            string Yoda = "";
-
-            WebRequest request = WebRequest.Create($"https://yoda.p.mashape.com/yoda?sentence={sent}");
-            request.Headers.Add("X-Mashape-Key", "s9SQZi4fYCmshpkI3i0IYEYg4m8hp13rW2DjsnALWln7XkT8Ez");
-
-            WebResponse response = request.GetResponse();
-            using (var streamReader = new StreamReader(response.GetResponseStream()))
-            {
-                Yoda = streamReader.ReadToEnd();
+            if (YodaInput != "")
+            { 
+                string Yoda = "";
+                WebRequest request = WebRequest.Create($"https://yoda.p.mashape.com/yoda?sentence={sent}");
+                request.Headers.Add("X-Mashape-Key", "s9SQZi4fYCmshpkI3i0IYEYg4m8hp13rW2DjsnALWln7XkT8Ez");
+                WebResponse response = request.GetResponse();
+                using (var streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    Yoda = streamReader.ReadToEnd();
+                }
+                return Yoda;
             }
-            return Yoda;
+            else
+            {
+                return "Found, no word is.";
+            }
+
         }
     }
 }
